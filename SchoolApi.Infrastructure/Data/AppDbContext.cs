584e084a6484c5ce6771b39068e2d3618a1f0f1a
youssef mohamed <youssef.mohamed.developer@gmail.com>
2025-06-22 00:05:39 +0300

    Add project files.

---
+++ SchoolApi.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApi.Data.Entities;
+
+namespace SchoolApi.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Department> departments { get; set; }
+        public DbSet<DepartmetSubject> DepartmetSubjects { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+        public DbSet<Subjects> Subjects { get; set; }
+
+
+    }
+}
