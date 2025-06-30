4a6067672fa0e113704546e007755bbeeb43140e
youssef mohamed <youssef.mohamed.developer@gmail.com>
2025-07-01 01:34:54 +0300

    added Response and Response Handler to my endpoint

--- SchoolApi.Core/Features/Students/Queries/Handlers/StudentHandler.cs
+++ SchoolApi.Core/Features/Students/Queries/Handlers/StudentHandler.cs
@@ -1,12 +1,13 @@
 ﻿using AutoMapper;
 using MediatR;
+using SchoolApi.Core.Bases;
 using SchoolApi.Core.Features.Students.Queries.Models;
 using SchoolApi.Core.Features.Students.Queries.Results;
 using SchoolApi.Service.Abstract;
 
 namespace SchoolApi.Core.Features.Students.Queries.Handlers
 {
-    public class StudentHandler : IRequestHandler<GetStudentsQuery, List<GetStudentsListResponse>>
+    public class StudentHandler : ResponseHandler, IRequestHandler<GetStudentsQuery, Response<List<GetStudentsListResponse>>>
     {
         private readonly IStudentService _studentService;
         private readonly IMapper _mapper;
@@ -16,11 +17,11 @@ namespace SchoolApi.Core.Features.Students.Queries.Handlers
             _studentService = studentService;
             _mapper = mapper;
         }
-        public async Task<List<GetStudentsListResponse>> Handle(GetStudentsQuery request, CancellationToken cancellationToken)
+        public async Task<Response<List<GetStudentsListResponse>>> Handle(GetStudentsQuery request, CancellationToken cancellationToken)
         {
             var StudentLsit = await _studentService.GetStudentsAsync();
             var StudentListMapper = _mapper.Map<List<GetStudentsListResponse>>(StudentLsit);
-            return StudentListMapper;
+            return Success(StudentListMapper);
         }
     }
 }
--- SchoolApi.Core/Features/Students/Queries/Models/GetStudentsQuery.cs
+++ SchoolApi.Core/Features/Students/Queries/Models/GetStudentsQuery.cs
@@ -1,9 +1,10 @@
 ﻿using MediatR;
+using SchoolApi.Core.Bases;
 using SchoolApi.Core.Features.Students.Queries.Results;
 
 namespace SchoolApi.Core.Features.Students.Queries.Models
 {
-    public class GetStudentsQuery : IRequest<List<GetStudentsListResponse>>
+    public class GetStudentsQuery : IRequest<Response<List<GetStudentsListResponse>>>
     {
 
     }
